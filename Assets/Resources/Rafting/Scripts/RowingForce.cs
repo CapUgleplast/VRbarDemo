15a790e8f6e345ad11702272049b610b72f86597
CapUgleplast <kpowdm@yandex.ru>
2021-07-05 21:47:30 +0700

    BadPaddle

---
+++ Assets/Resources/Rafting/Scripts/RowingForce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowingForce : MonoBehaviour
+{
+    private Vector3 startPoint;
+    private Vector3 tmpPoint;
+    private Vector3 impulseVector;
+    public Rigidbody SqrWorld;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Paddle") {
+            startPoint = other.transform.position;
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Paddle") {
+            tmpPoint = other.transform.position;
+            impulseVector = (tmpPoint - startPoint);
+            SqrWorld.AddForce(impulseVector*20, ForceMode.Impulse);
+            startPoint = tmpPoint;
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
