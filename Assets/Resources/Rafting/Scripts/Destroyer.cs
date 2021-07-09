4a677ddabc493b5b12b4f057685010524835645d
fima212 <212fima@mail.ru>
2021-07-09 18:26:49 +0700

    BadHammer

--- Assets/Resources/Rafting/Scripts/Destroyer.cs
+++ Assets/Resources/Rafting/Scripts/Destroyer.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
+    public bool FixPoint1 = false;
+    public bool FixPoint2 = false;
+    public GameObject Breakdown;
 
     private void OnTriggerEnter(Collider other) {
-        Destroy(other);
+        if (other.tag == "FixPoint1") {
+            FixPoint1 = true;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "FixPoint2") {
+            FixPoint2 = true;
+            Destroy(other.gameObject);
+        }
+
+    }
+    void Update() {
+        if (FixPoint1 == true && FixPoint2 == true){
+            Destroy(Breakdown);
+        }
     }
 }
